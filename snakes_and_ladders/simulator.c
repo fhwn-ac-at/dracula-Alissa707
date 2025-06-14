276b5e843cc46c50b8666dee0dba3eba4c66baf7
Alissa <alissalehner2003@gmail.com>
2025-06-14 10:03:56 +0200

    Simulationslogik zum Sammeln von Spielstatistiken implementiert

--- snakes_and_ladders/simulator.c
+++ snakes_and_ladders/simulator.c
@@ -27,4 +27,42 @@ static void track_usage(TransitionUsage *usage, int *count, int square) {
     (*count)++;
 }
 
+void run_simulations(const Board *board, int num_simulations, int die_sides, bool must_land_exactly) {
+    int total_rolls = 0;
+    int shortest_win = MAX_ROLLS;
+    int shortest_sequence[MAX_ROLLS];
+    int snake_usage_count = 0, ladder_usage_count = 0;
+    TransitionUsage snake_usage[MAX_TRANSITIONS] = {0};
+    TransitionUsage ladder_usage[MAX_TRANSITIONS] = {0};
 
+    for (int sim = 0; sim < num_simulations; ++sim) {
+        GameState state = init_game();
+        int rolls[MAX_ROLLS];
+        int roll_count = 0;
+
+        while (!has_won(&state, board) && roll_count < MAX_ROLLS) {
+            int roll = roll_die(die_sides);
+            int prev = state.position;
+            apply_move(&state, board, roll, must_land_exactly);
+            rolls[roll_count++] = roll;
+
+            if (board->transitions[prev + roll] != -1) {
+                int from = prev + roll;
+                int to = board->transitions[from];
+
+                if (to > from) {
+                    track_usage(ladder_usage, &ladder_usage_count, from);
+                } else {
+                    track_usage(snake_usage, &snake_usage_count, from);
+                }
+            }
+        }
+
+        if (has_won(&state, board)) {
+            total_rolls += roll_count;
+            if (roll_count < shortest_win) {
+                shortest_win = roll_count;
+                memcpy(shortest_sequence, rolls, sizeof(int) * roll_count);
+            }
+        }
+    }
