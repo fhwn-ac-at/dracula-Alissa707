f02cbe22fe97f7baa4d49b45ac0bea1fa824a16b
Alissa <alissalehner2003@gmail.com>
2025-06-14 10:34:36 +0200

    Befehlszeilenargumente für die Simulationskonfiguration analysieren

--- snakes_and_ladders/main.c
+++ snakes_and_ladders/main.c
@@ -20,3 +20,38 @@ void print_usage(const char *progname) {
     printf("  --help           Show this help message\n");
 }
 
+int main(int argc, char *argv[]) {
+    int num_games = 1000;
+    int die_sides = 6;
+    int must_exact = 0;
+
+    static struct option long_options[] = {
+        {"games", required_argument, 0, 'g'},
+        {"die", required_argument, 0, 'd'},
+        {"exact", no_argument, 0, 'e'},  // <- geändert
+        {"help", no_argument, 0, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    int opt_index = 0;
+    while ((opt = getopt_long(argc, argv, "g:d:h", long_options, &opt_index)) != -1) {
+        switch (opt) {
+            case 'g':
+                num_games = atoi(optarg);
+            break;
+            case 'd':
+                die_sides = atoi(optarg);
+            break;
+            case 'e':
+                must_exact = 1;
+            break;
+            case 'h':
+                print_usage(argv[0]);
+            return 0;
+            default:
+                print_usage(argv[0]);
+            return 1;
+        }
+    }
+
