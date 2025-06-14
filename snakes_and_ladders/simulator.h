97c963cecb64db56e0ed4daf15131e599cad0697
Alissa <alissalehner2003@gmail.com>
2025-06-14 09:48:51 +0200

    Header für simulation interface hinzugefügt

---
+++ snakes_and_ladders/simulator.h
@@ -0,0 +1,15 @@
+//
+// Created by aliss on 13.06.2025.
+//
+
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#include "board.h"
+#include "game.h"
+#include <stdbool.h>
+
+void run_simulations(const Board *board, int num_simulations, int die_sides, bool must_land_exactly);
+
+#endif // SIMULATOR_H
+
