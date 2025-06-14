dd5ea56faecf1bea5aaa79ea63824e8a5626a67f
Alissa <alissalehner2003@gmail.com>
2025-06-14 09:23:19 +0200

    Eine Würfelwurffunktion hinzugefügt, die einen Wert von 1 bis n zurückgibt

--- snakes_and_ladders/game.c
+++ snakes_and_ladders/game.c
@@ -3,12 +3,17 @@
 //
 
 #include "game.h"
+#include <stdlib.h>
 
 GameState init_game() {
     GameState state;
     state.position = -1;  // Player starts before the board
     state.steps = 0;
     return state;
+}
+
+int roll_die(int sides) {
+    return (rand() % sides) + 1;  // Returns value from 1 to sides
 }
 
 
--- snakes_and_ladders/game.h
+++ snakes_and_ladders/game.h
@@ -6,13 +6,16 @@
 #define GAME_H
 
 #include "board.h"
+#include <stdbool.h>
 
+// Represents the current state of a game
 typedef struct {
-    int position;
-    int steps;
+    int position;    // Current position on the board (0-based index)
+    int steps;       // Number of dice rolls taken so far
 } GameState;
 
 GameState init_game();
 
-#endif // GAME_H
+int roll_die(int sides);
 
+#endif // GAME_H
